d9aa48deb24779b969d56d750b125dc64ad7bd72
mayboroda.d <mayboroda.d@granit-concern.ru>
2026-02-13 18:02:33 +0300

    fix: remove --skip-generate from entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running Prisma db push..."
-prisma db push --skip-generate --schema=./prisma/schema.prisma
+prisma db push --schema=./prisma/schema.prisma
 
 echo "Running seed..."
 prisma db seed || echo "Seed skipped or already done"
